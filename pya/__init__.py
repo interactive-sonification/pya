454f483fd5739e01f7c4051a8a0d36a452bab38c
wiccy46 <eric1986_chn@yahoo.com>
2019-04-03 13:52:19 +0200

    add Soundserver

--- pya/__init__.py
+++ pya/__init__.py
@@ -1,6 +1,6 @@
 # """Collection of classes and functions for processing audio signals 
 # in python and jupyter notebooks, for synthesis, effects, analysis and plotting.
 # """
-from .pya import Asig, Aspec, Astft
+from .pya import Asig, Aspec, Astft, Soundserver
 from .helpers import ampdb, dbamp, cpsmidi, midicps, linlin, clip, record, playpyaudio
 from .pyaudiostream import *
