07a5b0bab4e12659ce10b3c45405941359c023b1
wiccy46 <eric1986_chn@yahoo.com>
2019-04-10 11:31:44 +0200

    timeslice now in the form of a simple tuple.

--- pya/pya.py
+++ pya/pya.py
@@ -17,10 +17,6 @@ from .pyaudiostream import PyaudioStream
 
 from .helpers import ampdb, linlin, dbamp, timeit
 
-
-# def ts(t0, t1, step):
-#     return {'tslice': [t0, t1, step]}
-
 class Asig:
     'audio signal class'
     def __init__(self, sig, sr=44100, label="", channels=1, cn=None):
@@ -97,10 +93,6 @@ class Asig:
             else:
                 raise TypeError("column names need to be a list of strings")
 
-    @staticmethod
-    def ts(t0, t1, step):
-        return {'tslice': [t0, t1, step]}
-
 
     def __getitem__(self, index):
         """
@@ -133,9 +125,14 @@ class Asig:
                 sr = int(self.sr/abs(step))
                 rslice = index[0] # row slice
 
-            elif isinstance(index[0], dict) and 'tslice' in index[0].keys():
-                tstart, tstop, step = index[0]['tslice']
-                rslice = slice(int(tstart*self.sr), int(tstop*self.sr), step)
+            elif isinstance(index[0], tuple): #time slice with tuple (start,end) or (start,end,step)
+                if len(index[0]) == 2:
+                    tstart, tstop = index[0]
+                    step = 1
+                    rslice = slice(int(tstart*self.sr), int(tstop*self.sr), step)
+                else:
+                    tstart, tstop, step = index[0]
+                    rslice = slice(int(tstart*self.sr), int(tstop*self.sr), step)
                 sr = int(self.sr/abs(step))
 
             else:
