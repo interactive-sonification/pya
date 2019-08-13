8974808bbc942201d903043b30f718a44ebf9f5b
wiccy46 <eric1986_chn@yahoo.com>
2019-08-13 17:23:02 +0200

    add test file for file load.

--- tests/test_file_loader.py
+++ tests/test_file_loader.py
@@ -1,13 +1,23 @@
 from pya import Asig
 from unittest import TestCase
 
+
 class TestLoadFile(TestCase):
     """Test loading audio file."""
-    
     def setUp(self):
         pass
 
     def tearDown(self):
         pass
 
-    def test_events(self):
+    def test_wav(self):
+        asig = Asig("./examples/samples/stereoTest.wav")
+        self.assertEqual(2, asig.channels)
+
+    def test_aiff(self):
+        asig = Asig("./examples/samples/notes_sr32000_stereo.aif")
+        self.assertEqual(32000, asig.sr)
+
+    def test_mp3(self):
+        asig = Asig("./examples/samples/ping.mp3")
+        self.assertEqual(34158, asig.samples)
