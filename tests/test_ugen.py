f8128a57652ebd220d81230c610521649c3f3f65
wiccy46 <eric1986_chn@yahoo.com>
2019-09-24 16:29:21 +0200

    square sample_shift, reimplement get row numbers

--- tests/test_ugen.py
+++ tests/test_ugen.py
@@ -18,9 +18,18 @@ class TestUgen(TestCase):
         self.assertEqual(44100 // 2, sine.sr)
         self.assertAlmostEqual(0.5, np.max(sine.sig), places=6)
         self.assertEqual((44100 // 2, 2), sine.sig.shape)
+        
+        sine = Ugen().sine(freq=200, amp=0.5, n_rows= 44100 // 2, sr=44100 // 2, channels=2)
+        self.assertEqual(44100 // 2, sine.sr)
+        self.assertAlmostEqual(0.5, np.max(sine.sig), places=6)
+        self.assertEqual((44100 // 2, 2), sine.sig.shape)
 
     def test_cos(self):
         cos = Ugen().cos(freq=200, amp=0.5, dur=1.0, sr=44100 // 2, channels=2)
+        self.assertEqual(44100 // 2, cos.sr)
+        self.assertAlmostEqual(0.5, np.max(cos.sig), places=6)
+        self.assertEqual((44100 // 2, 2), cos.sig.shape)
+        cos = Ugen().cos(freq=200, amp=0.5, n_rows=44100 // 2, sr=44100 // 2, channels=2)
         self.assertEqual(44100 // 2, cos.sr)
         self.assertAlmostEqual(0.5, np.max(cos.sig), places=6)
         self.assertEqual((44100 // 2, 2), cos.sig.shape)
@@ -32,11 +41,24 @@ class TestUgen(TestCase):
         self.assertEqual((44100 // 2, 2), square.sig.shape)
 
     def test_sawooth(self):
-        saw = Ugen().sawtooth(freq=200, amp=0.5, dur=1.0, sr=44100 // 2, channels=2)
+        saw = Ugen().sawtooth(freq=200, amp=0.5, dur=1, sr=44100 // 2, channels=2)
         self.assertEqual(44100 // 2, saw.sr)
         self.assertAlmostEqual(0.5, np.max(saw.sig), places=6)
         self.assertEqual((44100 // 2, 2), saw.sig.shape)
 
     def test_noise(self):
-        white = Ugen().noise(type="white")
+        white = Ugen().noise(type="white", amp=0.2, dur=1.0, sr=1000, cn=['white'], label='white_noise')
         pink = Ugen().noise(type="pink")
+        self.assertEqual(white.sr, 1000)
+        self.assertAlmostEqual(np.max(white.sig), 0.2, places=3)
+        self.assertAlmostEqual(white.get_duration(), 1.0, places=7)
+        self.assertEqual(white.cn, ['white'])
+        self.assertEqual(white.label, 'white_noise')
+        white_2ch = Ugen().noise(type="pink", channels=2)
+        self.assertEqual(white_2ch.channels, 2)
+        
+    def test_dur_n_rows_exception(self):
+        # An exception should be raised if both dur and n_rows are define. 
+        with self.assertRaises(AttributeError):
+            asig = Ugen().sine(dur=1., n_rows=400)
+        
