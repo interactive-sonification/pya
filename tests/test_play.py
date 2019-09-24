22bbcfa0d356033d7f8156e6d6f41c65fa1dc8b9
wiccy46 <eric1986_chn@yahoo.com>
2019-09-24 13:47:34 +0200

    add mock test

--- tests/test_play.py
+++ tests/test_play.py
@@ -1,5 +1,5 @@
 import time
-from unittest import TestCase, skipUnless
+from unittest import TestCase, skipUnless, mock
 from pya import *
 import numpy as np
 import pyaudio
@@ -11,6 +11,15 @@ try:
     has_output = True
 except OSError:
     pass
+
+class MockAudio(mock.MagicMock):
+   
+    channels_in = 1
+    channels_out = 4
+    
+    def get_device_info_by_index(self, *args):
+        return {'maxInputChannels': self.channels_in, 'maxOutputChannels': self.channels_out,
+                'name': 'MockAudio', 'index': 42}
 
 
 class TestPlay(TestCase):
@@ -27,13 +36,45 @@ class TestPlay(TestCase):
     def tearDown(self):
         pass
 
+    # @skipUnless(has_output, "PyAudio found no output device.")
+    def test_mock_play(self):
+
+        # Shift a mono signal to chan 4 should result in a 4 channels signals
+        mock_audio = MockAudio()
+        with mock.patch('pyaudio.PyAudio', return_value=mock_audio):
+            s = Aserver()
+            s.boot()
+            assert mock_audio.open.called
+            # since default AServer channel output is stereo we expect open to be called with
+            # channels=2 
+            self.assertEqual(mock_audio.open.call_args_list[0][1]["channels"], 2)
+            d1 = np.linspace(0, 1, 44100)
+            d2 = np.linspace(0, 1, 44100)
+            asig = Asig(d1)
+            s.play(asig)
+            self.assertTrue(np.allclose(s.srv_asigs[0].sig, d2.reshape(44100,1)))
+        with mock.patch('pyaudio.PyAudio', return_value=mock_audio):
+            s = Aserver(channels=6)
+            s.boot()
+            # AServer should reduce channels to 4 since MckAudio only pprovides 
+            # 4 output channels
+            assert mock_audio.open.call_count == 2
+            self.assertEqual(mock_audio.open.call_args_list[1][1]["channels"], 4)
+
     @skipUnless(has_output, "PyAudio found no output device.")
     def test_play(self):
-        # Shift a mono signal to chan 4 should result in a 4 channels signals
         s = Aserver()
         s.boot()
-        self.asine.gain(db=-3).play(server=s)
+        self.asine.play(server=s)
         time.sleep(2)
+
+    @skipUnless(has_output, "PyAudio found no output device.")   
+    def test_stop(self):
+        s = Aserver()
+        s.boot()
+        self.asine.play(server=s)
+        s.stop()
+        s.quit()
 
     def test_gain(self):
         result = (self.asine * 0.2).sig
@@ -48,3 +89,5 @@ class TestPlay(TestCase):
         # This test currently only check if there is error running the code, but not whether resampling is correct
         result = self.asine.resample(target_sr=44100 // 2, rate=1, kind='linear')
         self.assertIsInstance(result, Asig)
+
+
