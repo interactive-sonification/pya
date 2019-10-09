5966507ea15e4841283d6fc905c22b08a7864d85
Alexander Neumann <aleneum@gmail.com>
2019-10-09 10:17:30 +0200

    adjust tests to new backend structure

--- tests/test_arecorder.py
+++ tests/test_arecorder.py
@@ -37,13 +37,32 @@ FAKE_OUTPUT = {'index': 1,
                'defaultHighOutputLatency': 0.04,
                'defaultSampleRate': 44100.0}
 
+FAKE_AUDIO_INTERFACE = {'index': 2,
+                        'structVersion': 2,
+                        'name': 'Mock Audio Interface',
+                        'hostApi': 0,
+                        'maxInputChannels': 14,
+                        'maxOutputChannels': 14,
+                        'defaultLowInputLatency': 0.01,
+                        'defaultLowOutputLatency': 0.02,
+                        'defaultHighInputLatency': 0.03,
+                        'defaultHighOutputLatency': 0.04,
+                        'defaultSampleRate': 48000.0}
+
 
 class MockRecorder(mock.MagicMock):
     def get_device_count(self):
         return 2
 
     def get_device_info_by_index(self, arg):
-        return FAKE_INPUT if arg == 0 else FAKE_OUTPUT
+        if arg == 0:
+            return FAKE_INPUT
+        elif arg == 1:
+            return FAKE_OUTPUT
+        elif arg == 2:
+            return FAKE_AUDIO_INTERFACE
+        else:
+            raise AttributeError("Invalid device index.")
 
     def get_default_input_device_info(self):
         return FAKE_INPUT
@@ -52,17 +71,14 @@ class MockRecorder(mock.MagicMock):
         return FAKE_OUTPUT
 
 
-class TestArecorder(TestCase):
+class TestArecorderBase(TestCase):
 
-    def setUp(self):
-        pass
-
-    def tearDown(self):
-        pass
+    __test__ = False
+    backend = None
 
     @skipUnless(has_input, "PyAudio found no input device.")
     def test_arecorder(self):
-        ar = Arecorder(channels=1).boot()
+        ar = Arecorder(channels=1, backend=self.backend).boot()
         self.assertEqual(ar.sr, 44100)
         ar.record()
         time.sleep(1.)
@@ -77,13 +93,21 @@ class TestArecorder(TestCase):
         ar.recordings.clear()
         ar.quit()
 
+
+class TestArecorder(TestArecorderBase):
+
+    __test__ = True
+
+
+class TestMockArecorder(TestCase):
+
     def test_mock_arecorder(self):
         mock_recorder = MockRecorder()
         with mock.patch('pyaudio.PyAudio', return_value=mock_recorder):
             ar = Arecorder()
             self.assertEqual(
-                "Mock Input", 
-                ar.pa.get_default_input_device_info()['name'])
+                "Mock Input",
+                ar.backend.get_default_input_device_info()['name'])
             ar.boot()
             self.assertTrue(mock_recorder.open.called)
             ar.record()
@@ -92,5 +116,5 @@ class TestArecorder(TestCase):
             ar.record()
             ar.recordings.clear()
             self.assertEqual(0, len(ar.recordings))
-            # ar.stop()  # Dont know how to mock the stop. 
-            # TODO How to mock a result. 
+            # ar.stop()  # Dont know how to mock the stop.
+            # TODO How to mock a result.
