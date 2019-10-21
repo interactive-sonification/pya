e15b303455780f5441bba7ef7c08b21d8465f049
Alexander Neumann <aleneum@gmail.com>
2019-10-21 21:11:10 +0200

    remove record; cleanup import statements in (helpers).__init__
    * also import backends into pya namespace automatically.

--- pya/helper/__init__.py
+++ pya/helper/__init__.py
@@ -1,9 +1,3 @@
 from .helpers import ampdb, dbamp, cpsmidi, midicps, linlin, clip
 from .helpers import audio_from_file, buf_to_float, spectrum, audio_from_file
-from .helpers import normalize, record, device_info, find_device
-
-
-__all__ = ['ampdb', 'dbamp', 'cpsmidi', 'midicps', 'linlin', 
-           'clip', 'audio_from_file', 'buf_to_float', 
-           'spectrum', 'audio_from_file', 'normalize', 'record',
-           'device_info', 'find_device']
+from .helpers import normalize, device_info, find_device
