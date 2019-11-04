4501928175a867fbad9150d89b9a0803914c040c
wiccy46 <eric1986_chn@yahoo.com>
2019-11-04 15:58:19 +0100

    add numpy docstring style to documentation

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,9 @@ release = '0.3.2'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'm2r']
+extensions = ['sphinx.ext.autodoc', 
+              'sphinx.ext.napoleon',
+              'm2r']
 
 
 source_suffix = ['.rst', '.md']
