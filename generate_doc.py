e45cb616b6bd300e794b3bb19ff0329a68320197
Alexander Neumann <aleneum@gmail.com>
2019-11-15 11:01:38 +0100

    generate index.html

--- generate_doc.py
+++ generate_doc.py
@@ -11,11 +11,11 @@ VERSION_FILE = "versions.html"
 def generate(args):
     out = subprocess.check_output("git show-ref --tags -d".split(' '))
     tags = ['tags/' + str(line.split(b' ')[1].split(b'/')[2], 'utf-8') for line in out.split(b'\n')[:-1]]
-    tags += ['master', 'develop']
+    branches = ['develop', 'master']
     generated = []
     if not exists(TEMP_FOLDER):
         os.system(f'git clone https://github.com/interactive-sonification/pya.git {TEMP_FOLDER}')
-    for t in tags:
+    for t in branches + tags:
         os.system(f'git -C {TEMP_FOLDER} checkout {t}')
         if exists(f'{TEMP_FOLDER}/docs/'):
             print(f'sphinx-build -b html -D version={t} {TEMP_FOLDER}/docs {BUILD_FOLDER}/{t}')
@@ -36,6 +36,14 @@ Choose a version:
 </ul>
 </body>
 </html>
+        """)
+    
+    default_version = generated[-1]
+    with open(f'{BUILD_FOLDER}/index.html', 'w') as fp:
+        fp.write(f"""<!DOCTYPE html>
+        <html><head>
+        <meta http-equiv="refresh" content="0; url={default_version}/index.html">
+        </head></html>
         """)
 
 if __name__ == "__main__":
