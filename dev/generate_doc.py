d0ab371fef3a6ef65d0e80979b4da4e183a477df
Alexander Neumann <aleneum@gmail.com>
2019-11-18 12:29:21 +0100

    introduce dropdown in documentation

--- dev/generate_doc.py
+++ dev/generate_doc.py
@@ -4,42 +4,34 @@ import os
 import subprocess
 from os.path import exists
 
-BUILD_FOLDER = 'build/html'
 TEMP_FOLDER = 'build/git'
+BUILD_FOLDER = f'{TEMP_FOLDER}/build/html'
 VERSION_FILE = "versions.html"
 
 
 def generate(args):
     out = subprocess.check_output("git show-ref --tags -d".split(' '))
-    tags = ['tags/' + str(line.split(b' ')[1].split(b'/')[2], 'utf-8') for line in out.split(b'\n')[:-1]]
+    tags = [str(line.split(b' ')[1].split(b'/')[2], 'utf-8') for line in out.split(b'\n')[:-1]]
     branches = ['develop', 'master']
     generated = []
     if not exists(TEMP_FOLDER):
         os.system(f'git clone https://github.com/interactive-sonification/pya.git {TEMP_FOLDER}')
-    for t in branches + tags:
+        os.makedirs(BUILD_FOLDER)
+    # first, check which documentation to generate
+    for t in branches + tags[::-1]:
+        if t not in branches:
+            t = 'tags/' + t
         os.system(f'git -C {TEMP_FOLDER} checkout {t}')
         if exists(f'{TEMP_FOLDER}/docs/'):
-            print(f'sphinx-build -b html -D version={t} {TEMP_FOLDER}/docs {BUILD_FOLDER}/{t}')
-            os.system(f'sphinx-build -b html -D version={t} {TEMP_FOLDER}/docs {BUILD_FOLDER}/{t}')
             generated.append(t)
         else:
-            print(f'{t} has NOOOOOOO docs')
-
-    version_html_list = ['<li><a href="' + t + '"> ' + t + '</li>' for t in generated]
-    with open(f'{BUILD_FOLDER}/{VERSION_FILE}', 'w') as fp:
-        fp.write(f"""<!DOCTYPE html>
-<html>
-<body>
-<h2>pya Documentation</h2>
-Choose a version:
-<ul>
-{' '.join(version_html_list)}
-</ul>
-</body>
-</html>
-        """)
-
-    default_version = generated[-1]
+            print(f'{t} has no docs!')
+    # generate documentation; all versions have to be known for the dropdown menu
+    for t in generated:
+        os.system(f'git -C {TEMP_FOLDER} checkout {t}')
+        os.system(f'sphinx-build -b html -D version={t} -A versions={",".join(generated)} {TEMP_FOLDER}/docs {BUILD_FOLDER}/{t}')
+    # create index html to forward to last tagged version
+    default_version = generated[len(branches)] if len(generated) > len(branches) else generated[-1]
     with open(f'{BUILD_FOLDER}/index.html', 'w') as fp:
         fp.write(f"""<!DOCTYPE html>
         <html><head>
