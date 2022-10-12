7675b8e7f970a6d9f016da396b5e7d86dd250fb0
jiajun <eric1986_chn@yahoo.com>
2022-10-12 10:16:56 +0200

    Change test precision in ugen

--- tests/test_ugen.py
+++ tests/test_ugen.py
@@ -14,33 +14,33 @@ class TestUgen(TestCase):
     def test_sine(self):
         sine = Ugen().sine(freq=200, amp=0.5, dur=1.0, sr=44100 // 2, channels=2)
         self.assertEqual(44100 // 2, sine.sr)
-        self.assertAlmostEqual(0.5, np.max(sine.sig), places=6)
+        self.assertAlmostEqual(0.5, np.max(sine.sig), places=5)
         self.assertEqual((44100 // 2, 2), sine.sig.shape)
         sine = Ugen().sine(freq=200, amp=0.5, n_rows=44100 // 2, sr=44100 // 2, channels=2)
         self.assertEqual(44100 // 2, sine.sr)
-        self.assertAlmostEqual(0.5, np.max(sine.sig), places=6)
+        self.assertAlmostEqual(0.5, np.max(sine.sig), places=5)
         self.assertEqual((44100 // 2, 2), sine.sig.shape)
 
     def test_cos(self):
         cos = Ugen().cos(freq=200, amp=0.5, dur=1.0, sr=44100 // 2, channels=2)
         self.assertEqual(44100 // 2, cos.sr)
-        self.assertAlmostEqual(0.5, np.max(cos.sig), places=6)
+        self.assertAlmostEqual(0.5, np.max(cos.sig), places=5)
         self.assertEqual((44100 // 2, 2), cos.sig.shape)
         cos = Ugen().cos(freq=200, amp=0.5, n_rows=44100 // 2, sr=44100 // 2, channels=2)
         self.assertEqual(44100 // 2, cos.sr)
-        self.assertAlmostEqual(0.5, np.max(cos.sig), places=6)
+        self.assertAlmostEqual(0.5, np.max(cos.sig), places=5)
         self.assertEqual((44100 // 2, 2), cos.sig.shape)
 
     def test_square(self):
         square = Ugen().square(freq=200, amp=0.5, dur=1.0, sr=44100 // 2, channels=2)
         self.assertEqual(44100 // 2, square.sr)
-        self.assertAlmostEqual(0.5, np.max(square.sig), places=6)
+        self.assertAlmostEqual(0.5, np.max(square.sig), places=5)
         self.assertEqual((44100 // 2, 2), square.sig.shape)
 
     def test_sawooth(self):
         saw = Ugen().sawtooth(freq=200, amp=0.5, dur=1, sr=44100 // 2, channels=2)
         self.assertEqual(44100 // 2, saw.sr)
-        self.assertAlmostEqual(0.5, np.max(saw.sig), places=6)
+        self.assertAlmostEqual(0.5, np.max(saw.sig), places=5)
         self.assertEqual((44100 // 2, 2), saw.sig.shape)
 
     def test_noise(self):
