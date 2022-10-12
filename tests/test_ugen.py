9c66a1606864ada15d1fbc5a59f956d4297211dc
jiajun <eric1986_chn@yahoo.com>
2022-10-12 10:16:56 +0200

    Change test precision in ugen

--- tests/test_ugen.py
+++ tests/test_ugen.py
@@ -37,13 +37,13 @@ class TestUgen(TestCase):
     def test_square(self):
         square = Ugen().square(freq=200, amp=0.5, dur=1.0, sr=44100 // 2, channels=2)
         self.assertEqual(44100 // 2, square.sr)
-        self.assertAlmostEqual(0.5, np.max(square.sig), places=6)
+        self.assertAlmostEqual(0.5, np.max(square.sig), places=5)
         self.assertEqual((44100 // 2, 2), square.sig.shape)
 
     def test_sawooth(self):
         saw = Ugen().sawtooth(freq=200, amp=0.5, dur=1, sr=44100 // 2, channels=2)
         self.assertEqual(44100 // 2, saw.sr)
-        self.assertAlmostEqual(0.5, np.max(saw.sig), places=6)
+        self.assertAlmostEqual(0.5, np.max(saw.sig), places=5)
         self.assertEqual((44100 // 2, 2), saw.sig.shape)
 
     def test_noise(self):
