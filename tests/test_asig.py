fe5e14d1fc77b651af36610588aa15c1e9082556
Thomas Hermann <thermann@techfak.uni-bielefeld.de>
2025-03-03 18:46:26 +0100

    fixed formatting

--- tests/test_asig.py
+++ tests/test_asig.py
@@ -246,12 +246,12 @@ class TestAsig(TestCase):
         self.assertEqual(a.append(b).sig.shape, (8,))
         with self.assertRaises(AttributeError):
             a.append(b.stereo())
-        self.assertEqual(a.stereo().append(b.stereo()).sig.shape, (8,2) )
+        self.assertEqual(a.stereo().append(b.stereo()).sig.shape, (8, 2))
         a = Asig([1, 2, 3], sr=2).stereo()
         b = Asig(np.array([[1, 0, 1], [0, 1, 0]]).T, sr=2)
         ab = b.append(a).sig
-        r = np.array([[1, 0],[0, 1],[1, 0],[1, 1],[2, 2],[3, 3]], dtype=np.float32)
-        self.assertEqual(np.max(np.abs(ab - r)), 0) 
+        r = np.array([[1, 0], [0, 1], [1, 0], [1, 1], [2, 2], [3, 3]], dtype=np.float32)
+        self.assertEqual(np.max(np.abs(ab - r)), 0)
 
     # # At the top I import Asig by: from pya import Asig
     @mock.patch("pya.asig.wavfile")
