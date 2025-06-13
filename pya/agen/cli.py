74de2b1f1af9a93dd899868574920a4f7bf0aa68
Luka Born <luka.born.2003@gmail.com>
2025-06-13 11:10:41 +0200

    update pya dependency in cli project template

--- pya/agen/cli.py
+++ pya/agen/cli.py
@@ -12,6 +12,7 @@ import modulefinder
 COMMANDS = {}
 
 PACKAGE_INDEX_URL = "https://gitlab.ub.uni-bielefeld.de/IchbinLuka/pya_index_test"
+PYA_DEPENDENCY = "pya[agen] @ git+https://github.com/interactive-sonification/pya@feature-agen"
 
 
 def command(name: str):
@@ -74,7 +75,7 @@ requires-python = ">=3.10"
 readme = "README.md"
 authors = []
 dependencies = [
-    "pya[agen] @ git+https://github.com/IchbinLuka/pya@develop",
+    {PYA_DEPENDENCY},
 ]
 """
         )
