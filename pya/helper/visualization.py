b3ab1d8884f5e2d17c0942f47e04cb659c7de880
HalbFettKaese <p.wardermann@outlook.de>
2025-11-21 15:12:11 +0100

    Fix plotting to custom axis

--- pya/helper/visualization.py
+++ pya/helper/visualization.py
@@ -28,7 +28,7 @@ def basicplot(data: np.ndarray, ticks, channels, offset=0, scale=1,
             Plot type.
 
     """
-    ax = plt.gca() or ax
+    ax = ax or plt.gca()
     if channels == 1 or (offset == 0 and scale == 1):
         # if mono signal or you would like to stack signals together
         # offset is the spacing between channel,
