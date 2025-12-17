69037b1445172f71744f84ae4077d62af1d4b35f
HalbFettKaese <p.wardermann@outlook.de>
2025-12-17 16:40:19 +0100

    Terminate backend when quitting server

--- pya/aserver.py
+++ pya/aserver.py
@@ -226,6 +226,7 @@ class Aserver:
         except AttributeError:
             _LOGGER.info("No stream found...")
         self.stream = None
+        self.backend.terminate()
 
     def play(self, asig, onset: Union[int, float] = 0, out: int = 0, **kwargs):
         """Dispatch asigs or arrays for given onset.
@@ -407,14 +408,12 @@ class Aserver:
         """Context manager exit"""
         _LOGGER.info("Exiting context manager. Cleaning up stream and backend")
         self.quit()
-        self.backend.terminate()
 
     def __del__(self):
         """Backup cleanup, only if context manager wasn't used"""
         if hasattr(self, 'stream') and self.stream is not None:
             try:
                 self.quit()
-                self.backend.terminate()
             except:
                 pass  # Ignore cleanup errors during shutdown
 
